0dac75cd1fb949a242bd4672014d4dce4a4cfdb9
stofstik <verstegen.daan@gmail.com>
2015-08-17 17:42:53 +0200

    SystemUI: Add tile to show volume panel
    Squashed:
    
        From: Joey Huab <joey@evolution-x.org>
        Date: Wed, 18 Mar 2020 16:23:51 +0000
        Subject: QS: Use Settings.Panel intent for Volume Tile
    
        Signed-off-by: Joey Huab <joey@evolution-x.org>
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Change-Id: Ic583c24e304d1edc903127237fc9eb5c7eeb7e4d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/sospos/SospOSModule.kt
+++ packages/SystemUI/src/com/android/systemui/sospos/SospOSModule.kt
@@ -24,6 +24,7 @@ import com.android.systemui.qs.tiles.CaffeineTile
 import com.android.systemui.qs.tiles.HeadsUpTile
 import com.android.systemui.qs.tiles.SoundTile
 import com.android.systemui.qs.tiles.UsbTetherTile
+import com.android.systemui.qs.tiles.VolumeTile
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
@@ -61,6 +62,12 @@ interface SospOSModule {
     @IntoMap
     @StringKey(UsbTetherTile.TILE_SPEC)
     fun bindUsbTetherTile(usbTetherTile: UsbTetherTile): QSTileImpl<*>
+
+    /** Inject VolumeTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(VolumeTile.TILE_SPEC)
+    fun bindVolumeTile(volumeTile: VolumeTile): QSTileImpl<*>
 
     /** Inject AODTile into tileMap in QSModule */
     @Binds
