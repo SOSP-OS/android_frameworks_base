6b77b2030b85cb9a92ae0ca6af6bd2fd70f127d5
gnathvm <gnathvm@gmail.com>
2019-12-29 19:50:51 +0530

    Add Simplx Utils

---
+++ core/java/com/android/internal/util/simplx/SimplxUtils.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2019 The Simplx AOSP Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.simplx;
+
+import android.os.PowerManager;
+import android.os.ServiceManager;
+import android.os.SystemClock;
+import android.content.Context;
+
+/**
+ * Add Simplx utilities
+ */
+public class SimplxUtils {
+
+    public static void switchScreenOff(Context ctx) {
+        PowerManager pm = (PowerManager) ctx.getSystemService(Context.POWER_SERVICE);
+        if (pm!= null) {
+            pm.goToSleep(SystemClock.uptimeMillis());
+        }
+    }
+}
