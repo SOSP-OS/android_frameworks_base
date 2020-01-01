3629b26fd7b0a8a72f7a2c9db692e4acedc37943
gnathvm <gnathvm@gmail.com>
2020-01-01 14:48:40 +0530

    Add api to take screenshots
    abc: extracted from DUI
    add a public call to SimplxUtils

--- core/java/com/android/internal/util/simplx/SimplxUtils.java
+++ core/java/com/android/internal/util/simplx/SimplxUtils.java
@@ -16,20 +16,36 @@
 
 package com.android.internal.util.simplx;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.PowerManager;
+import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.SystemClock;
-import android.content.Context;
+import android.view.IWindowManager;
+import android.view.WindowManagerGlobal;
 
 /**
  * Add Simplx utilities
  */
 public class SimplxUtils {
 
+    public static final String INTENT_SCREENSHOT = "action_handler_screenshot";
+    public static final String INTENT_REGION_SCREENSHOT = "action_handler_region_screenshot";
+
     public static void switchScreenOff(Context ctx) {
         PowerManager pm = (PowerManager) ctx.getSystemService(Context.POWER_SERVICE);
         if (pm!= null) {
             pm.goToSleep(SystemClock.uptimeMillis());
+        }
+    }
+
+    public static void takeScreenshot(boolean full) {
+        IWindowManager wm = WindowManagerGlobal.getWindowManagerService();
+        try {
+            wm.sendCustomAction(new Intent(full? INTENT_SCREENSHOT : INTENT_REGION_SCREENSHOT));
+        } catch (RemoteException e) {
+            e.printStackTrace();
         }
     }
 }
