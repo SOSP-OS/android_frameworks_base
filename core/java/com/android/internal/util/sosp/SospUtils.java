6a6472b59ec4e20713be854d3688d209eb55de2a
Gnathvm <Gnathvm@gmail.com>
2020-07-25 16:02:36 +0530

    base: fix build error

--- core/java/com/android/internal/util/sosp/SospUtils.java
+++ core/java/com/android/internal/util/sosp/SospUtils.java
@@ -20,7 +20,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Resources;
 import android.os.PowerManager;
 import android.os.RemoteException;
